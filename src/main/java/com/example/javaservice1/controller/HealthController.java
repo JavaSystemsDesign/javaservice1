056e6076b6a0e334ee36016240ae1923f69ba1b3
JavaSystemsDesign <eikbosserwegveedu@gmail.com>
2026-05-02 11:14:16 +0200

    healthendpointchange

--- src/main/java/com/example/javaservice1/controller/HealthController.java
+++ src/main/java/com/example/javaservice1/controller/HealthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/healthendpoint")
 public class HealthController {
 
     private static final Logger logger = LoggerFactory.getLogger(HealthController.class);
